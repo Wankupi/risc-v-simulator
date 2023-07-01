f401918dfbb75077feee50e08517907e0e8b9115
Wankupi <2893353848@qq.com>
2023-07-01 15:43:14 +0800

    random

--- src/cpu.h
+++ src/cpu.h
@@ -3,6 +3,9 @@
 #include "LoadStoreBuffer.h"
 #include "ReorderBuffer.h"
 #include "ReservationStation.h"
+#include <chrono>
+#include <functional>
+#include <random>
 
 class CPU {
 public:
@@ -24,13 +27,13 @@ public:
 		init();
 		unsigned long long tick = 0;
 		try {
-			for (; ; ++tick) {
+			for (;; ++tick) {
 				execute();
 				flush();
 			}
 		} catch (unsigned int x) {
 			std::cerr << "tick: " << tick << std::endl;
-			std::cerr << "predictor: "<< predictor.success << " / " << predictor.total << " = " << static_cast<long double>(predictor.success) / predictor.total << std::endl;
+			std::cerr << "predictor: " << predictor.success << " / " << predictor.total << " = " << static_cast<long double>(predictor.success) / predictor.total << std::endl;
 			return x;
 		} catch (...) {
 			throw;
@@ -39,14 +42,19 @@ public:
 	}
 	void execute() {
 		// up level
-		instruction.execute(*mem, !decoder.ready_next, decoder.set_PC, decoder.newPC, rob.clear_signal, rob.newPC);
-		decoder.execute(instruction, rob, rs, lsb, regs);
-		rob.execute(regs, rs.export_data(), lsb.export_data(), predictor);
-		lsb.execute(rs.export_data(), rob.data_to_LSB());
-		rs.execute(lsb.export_data(), rob.clear_signal);
-		cache.execute(rob.clear_signal);
-		alu.execute();
-		regs.execute(rob.clear_signal);
+		static int a[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+		static std::function<void()> func[] = {
+				[this]() { instruction.execute(*mem, !decoder.ready_next, decoder.set_PC, decoder.newPC, rob.clear_signal, rob.newPC); },
+				[this]() { decoder.execute(instruction, rob, rs, lsb, regs); },
+				[this]() { rob.execute(regs, rs.export_data(), lsb.export_data(), predictor); },
+				[this]() { lsb.execute(rs.export_data(), rob.data_to_LSB()); },
+				[this]() { rs.execute(lsb.export_data(), rob.clear_signal); },
+				[this]() { cache.execute(rob.clear_signal); },
+				[this]() { alu.execute(); },
+				[this]() { regs.execute(rob.clear_signal); }};
+		static std::default_random_engine e(std::chrono::system_clock::now().time_since_epoch().count());
+		std::shuffle(a, a + 8, e);
+		for (auto i: a) func[i]();
 	}
 	void flush() {
 		// down level
